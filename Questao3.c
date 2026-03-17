cb5d4de56dc0fdcce2efca7480a817de101d51b5
FabioTelles11 <fabiounijorge0@gmail.com>
2026-03-17 09:11:49 -0300

    Implement basic number summation program

---
+++ Questao3.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    int a,b;
+    float soma;
+    
+printf("Somador de numeros\n");
+
+printf("digite o primeiro numero:");
+scanf("%i",&a);
+
+printf("digite o segundo numero:");
+scanf("%i",&b);
+
+soma= a+b;
+
+printf("a soma dos numeros é: %2.f",soma);
+
+
+    return 0;
+}
