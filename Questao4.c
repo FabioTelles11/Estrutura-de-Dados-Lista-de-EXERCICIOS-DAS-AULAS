3f38561528aeba9b4f36667f50db1566e2c7f274
FabioTelles11 <fabiounijorge0@gmail.com>
2026-03-17 09:27:21 -0300

    Add multiplication program in Questao4.c

---
+++ Questao4.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    float a, b, multiplicacao;
+
+    
+printf("Multiplicador de numeros\n");
+
+printf("digite o primeiro numero: ");
+scanf("%f", &a);
+
+printf("digite o segundo numero: ");
+scanf("%f", &b);
+
+multiplicacao= a * b;
+
+printf("Resultado da multiplicacao: %.2f",multiplicacao);
+
+
+    return 0;
+}
